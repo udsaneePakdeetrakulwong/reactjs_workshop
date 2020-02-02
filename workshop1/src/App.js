bf939fc168f364878876ca248fac1a6775ff3c14
udsaneePakdeetrakulwong <60562490+udsaneePakdeetrakulwong@users.noreply.github.com>
2020-02-02 16:26:55 +0700

    react

---
+++ workshop1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> สวัสดี React</h1>
+      ทดสอบการทำงาน react js
+    </div>
+  );
+}
+
+export default App;
